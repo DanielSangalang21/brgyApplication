9dd69a8e110c945cfde7123eec8e5f84fab82691
Daniel <danielsangalang.21@gmail.com>
2023-04-16 22:13:11 +0800

    Modify htttp request to add JSON request body

--- src/main/java/HTTPRequest.java
+++ src/main/java/HTTPRequest.java
@@ -1,30 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import net.sf.jasperreports.data.http.RequestMethod;
+import net.sf.json.JSONObject;
 
 public class HTTPRequest {
 
 	private HttpURLConnection connection;
 	private URL url;
-	String strUrl = "http://my-tera.com/OTP_SYSTEM/Mobile/SystemList.htm";
-	
+	//String strUrl = "http://my-tera.com/OTP_SYSTEM/Mobile/SystemList.htm";
+	String strUrl = "https://dummyjson.com/auth/login";
 	HTTPRequest() throws MalformedURLException{
 		this.url = new URL(strUrl);
 	}
 	
 	public void sendRequest() throws IOException {
 		connection = (HttpURLConnection) url.openConnection();	
-		connection.setRequestMethod(RequestMethod.GET.name());
-		connection.setDoOutput(true);
-		OutputStream os = connection.getOutputStream();
+		connection.setRequestMethod(RequestMethod.POST.name());
+		connection.setRequestProperty("Content-Type", "application/json");
+		connection.setRequestProperty("Accept", "application/json");
 		
-		os.close();
+		JSONObject cred = new JSONObject();
+		cred.put("username","kminchelle");
+		cred.put("password", "0lelplR");
+		
+		connection.setDoOutput(true);
+		OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream());
+		osw.write(cred.toString());
+		osw.close();
 		int respCode = connection.getResponseCode();
 		BufferedReader br = new BufferedReader( new InputStreamReader(connection.getInputStream()) );
 		String content;
